daebefaddfd3eeed9b58478fce9b0a9efa796679
Manel-Tarhouni <m.tarhouni@instadeep.com>
2025-02-24 15:52:45 +0100

    initial commit

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AM.Infrastructure
+{
+    public class AMContext :DbContext
+    {
+        protected readonly IConfiguration configuration;
+        public AMContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        /*  public DbSet<User> Users { get; set; } 
+          public DbSet<Application> Applications { get; set; }
+          public DbSet<Role> Roles{ get; set; }
+
+          public DbSet<Token> Tokens { get; set; }*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("WebApiDatabase"));
+        }
+    }
+}
---
+++ Final_Year_Project_backend/Program.cs
@@ -0,0 +1,41 @@
+using AM.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+//await app.MigrateDatabase<DbContext>();
+//await app.RunAsync();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container BEFORE calling Build().
+builder.Services.AddControllers();
+
+// Register the DbContext (AMContext) with the DI container
+builder.Services.AddDbContext<AMContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))); 
+// Swagger setup
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); // Now we can call Build() after all services are registered.
+
+// Apply any pending migrations and create the database if needed
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AMContext>();
+    dbContext.Database.Migrate(); // This applies any pending migrations
+}
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers(); // Ensures your API controllers are mapped correctly
+
+//app.Run();
+await app.RunAsync();
